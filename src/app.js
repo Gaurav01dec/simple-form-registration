fe3de3714b7cd42a7a22538d2eaa7e59e3e6829e
unknown <tech.gauravverma@gmail.com~>
2024-03-27 19:56:03 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -15,27 +15,50 @@ const partialsPath = path.join(__dirname, "../templates/partials");
 // app.use(express.static(staticPath));
 //using template engine 
 app.set("view engine", "hbs");
-app.set("views",templatePath);
+app.set("views", templatePath);
 hbs.registerPartials(partialsPath);
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
     res.render("index.hbs");
 })
-app.post("/", async (req, res) => {
+app.post("/register", async (req, res) => {
     try {
         const registerEmployee = new Register({
-            name:req.body.name,
-            email:req.body.email,
-            number:req.body.number,
-            password:req.body.password,
+            name: req.body.name,
+            email: req.body.email,
+            number: req.body.number,
+            password: req.body.password,
         })
         registerEmployee.save();
         res.render("index.hbs")
     } catch (error) {
         res.send(error);
     }
+})
+
+app.post("/login", async (req, res) => {
+    const resultdata = await Register.find({ email: req.body.email })
+
+    if (resultdata.length == 0) {
+        res.send(`No record found ...Login Failed`)
+
+    } else{
+        nameinDatabase = await resultdata[0].name
+
+        passwordByUser = await req.body.password
+
+        if (passwordByUser == resultdata[0].password) {
+
+            res.send(`${nameinDatabase} ...Login Success`)
+        }else{
+            res.send(`Wrong Password ...Login Failed`)
+        }
+    }
+    // console.log(req.body.email);
+    // console.log(req.body.password);
+
 })
 
 app.listen(port, () => {
--- src/models/register.js
+++ src/models/register.js
@@ -7,6 +7,7 @@ const xyzSchema = new mongoose.Schema({
     },
     email:{
         type:String,
+        unique:true,
         required:true
     },
     number:{
@@ -20,6 +21,6 @@ const xyzSchema = new mongoose.Schema({
 })
 
 //Now we need to create our collection by using the schema 
-const Register = new mongoose.model("Register1",xyzSchema)
+const Register = new mongoose.model("Register2",xyzSchema)
 
 module.exports=Register
